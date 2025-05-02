abfa8831c671bd77b692b795664fff04f6fd95d0
sellen <austin0801@naver.com>
2025-05-02 16:40:46 +0900

    [feature] Test용 Micro Server 생성

---
+++ NormalClient/src/test/java/com/example/normalclient/NormalClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.normalclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NormalClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
