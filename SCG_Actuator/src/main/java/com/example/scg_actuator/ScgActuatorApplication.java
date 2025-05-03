aee316a3f04aa856e0a526fa04f8e35088927cab
sellen <austin0801@naver.com>
2025-05-03 23:03:40 +0900

    [feature] Spring Cloud GateWay Actuator 설정

---
+++ SCG_Actuator/src/main/java/com/example/scg_actuator/ScgActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.scg_actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScgActuatorApplication.class, args);
+    }
+
+}
